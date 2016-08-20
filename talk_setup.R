1c045f54d574fa594c00aa1523230056a999b59e
Courtney Campany <courtneycampany@gmail.com>
2016-08-20 12:07:41 +1000

    hopefully slifify works now

---
+++ talk_setup.R
@@ -0,0 +1,10 @@
+require(devtools)
+install_github("slidify", "ramnathv")
+install_github("slidifyLibraries", "ramnathv")
+install_github('ramnathv/rCharts', 'ramnathv', ref='dev')
+install_github('shiny', 'rstudio', force=TRUE)
+
+##first rstart Rstudio here
+install.packages('stringr')
+devtools::install_github('muschellij2/slidify')
+library(slidify)
